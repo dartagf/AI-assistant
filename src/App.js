5f9d2708df12456425ef5cc6043ecd225dfcf0ca
dartagf <entndjej@gmail.com>
2022-06-11 19:24:09 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+const alanKey =
+  "20e4a9f477a935a34175767f24ec24792e956eca572e1d8b807a3e2338fdd0dc/stage";
+const App = () => {
+  const [newsArticles, setNewsArticles] = useState([]);
+
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles }) => {
+        if (command === "newHeadLines") {
+          // alert("Ini adalah tes PR perintah!");
+          setNewsArticles(articles);
+        }
+      },
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>TESTING AI</h1>
+    </div>
+  );
+};
+
+export default App;
